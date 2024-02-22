db0f549d290f00918b7e3ad282f3e2fe9012f200
AraerTalsen <greenberg.dj@gmail.com>
2024-02-21 17:20:05 -0800

    Fixed Dialogue

--- Assets/Scripts/Interactables/NPCInteraction.cs
+++ Assets/Scripts/Interactables/NPCInteraction.cs
@@ -5,12 +5,10 @@ public class NPCInteraction : Interactable
 {
     public Transform playerTransform;
     public int context;
-    private TextManager tm;
 
     private void Start()
     {
         Player = playerTransform;
-        tm = GetComponent<TextManager>();
     }
 
     private void Update()
